a1651889ceb2a583d437380a887bc5ecf6311724
Angel Santoyo <santoyox714@gmail.com>
2023-09-26 13:45:29 -0700

    add error checking for creating students/classes
    -check if the student or class you are trying to make exists with id
    -add some utils that can be used for testing

--- routes.py
+++ routes.py
@@ -2,6 +2,10 @@ from fastapi import APIRouter
 from pydantic import BaseModel
 from typing import List
 
+from fastapi import Depends, HTTPException, APIRouter
+from typing import List
+from schemas import Student, Class  # Import your schemas
+
 router = APIRouter()
 
 class Class(BaseModel):
@@ -20,9 +24,13 @@ class Student(BaseModel):
 classes_db = []
 students_db = []
 
-# Define route endpoints and their functions
-@router.post("/classes/", response_model=Class)
+@router.post("/classes/", response_model=Student)
 def create_class(class_data: Class):
+    # Check if a class with the same course_code and section_number already exists
+    for existing_class in classes_db:
+        if existing_class.course_code == class_data.course_code and existing_class.section_number == class_data.section_number:
+            raise HTTPException(status_code=400, detail="Class with this code and section already exists")
+
     classes_db.append(class_data)
     return class_data
 
@@ -30,12 +38,19 @@ def create_class(class_data: Class):
 def list_classes():
     return classes_db
 
+
 @router.post("/students/", response_model=Student)
 def create_student(student_data: Student):
+    # Check if a student with the same student_id already exists
+    for existing_student in students_db:
+        if existing_student.student_id == student_data.student_id:
+            raise HTTPException(status_code=400, detail="Student with this ID already exists")
+
     students_db.append(student_data)
     return student_data
 
 @router.get("/students/", response_model=List[Student])
 def list_students():
     return students_db
+
 
---
+++ utils.py
@@ -0,0 +1,25 @@
+#routes not asked for but that may be useful for testing,
+
+
+
+#delete a student by student id (dropping students from a class may look similar)
+@router.delete("/students/{student_id}")
+def delete_student(student_id: str):
+    # Find the student by student_id
+    for student in students_db:
+        if student.student_id == student_id:
+            students_db.remove(student)
+            return {"message": "Student deleted successfully"}
+
+    raise HTTPException(status_code=404, detail="Student not found")
+
+
+#delete a class
+@router.delete("/classes/{course_code}-{section_number}")
+def delete_class(course_code: str, section_number: str):
+    for class_instance in classes_db:
+        if class_instance.course_code == course_code and class_instance.section_number == section_number:
+            classes_db.remove(class_instance)
+            return {"message": "Class deleted successfully"}
+
+    raise HTTPException(status_code=404, detail="Class not found")
